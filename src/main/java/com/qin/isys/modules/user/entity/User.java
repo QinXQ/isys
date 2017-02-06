c788f40654d8de4fdb5c8b75a01e3e5a15fdb9ac
qinxiaoqiang <qinxiaoqiangme@gmail.com>
2017-02-06 22:42:47 +0800

    一些简单的测试

---
+++ src/main/java/com/qin/isys/modules/user/entity/User.java
@@ -0,0 +1,16 @@
+package com.qin.isys.modules.user.entity;
+
+/**
+ * Created by Q on 2017/2/6.
+ */
+public class User {
+    private String uid;
+
+    public String getUid() {
+        return uid;
+    }
+
+    public void setUid(String uid) {
+        this.uid = uid;
+    }
+}
---
+++ src/main/java/com/qin/isys/springconfig/MainConfig.java
@@ -0,0 +1,10 @@
+package com.qin.isys.springconfig;
+
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by Q on 2017/2/6.
+ */
+@Configuration
+public class MainConfig {
+}
